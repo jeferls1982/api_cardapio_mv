4adfae8a2d625e0262239c0cbedc514878a6c0a6
gitJefersonSilveira <jeferson.silveira.github@hotmail.com>
2022-08-04 20:56:16 -0300

    teste outro pc

--- app/Http/Controllers/api/CardapioController.php
+++ app/Http/Controllers/api/CardapioController.php
@@ -29,7 +29,7 @@ class CardapioController extends BaseController
     public function index()
     {
         return $this->repository->list();
-   }
+    }
 
     /**
      * @OA\Post(
@@ -77,7 +77,6 @@ class CardapioController extends BaseController
         $sanitized = $request->all();
         return $this->repository->store($sanitized);
     }
-
 
     /**
      * @OA\Get(
